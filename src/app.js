a4580ab674ca22a1926fd889a20cc2e40ab0c3b1
cathrinsd <121065841+cathrinsd@users.noreply.github.com>
2023-01-12 12:34:11 -0500

    Added the 5day forecast in JS

--- src/app.js
+++ src/app.js
@@ -1,50 +1,72 @@
-
-
 function displayTemperature(response) {
-   let temperature = Math.round(response.data.temperature.current);
-   let temperatureElement = document.querySelector("#temperature");
-   temperatureElement.innerHTML =`${temperature}`;
-   celsiusTemperature = response.data.temperature.current;
-   let description = document.querySelector("#weather-description");
+  let temperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = `${temperature}`;
+  celsiusTemperature = response.data.temperature.current;
+  let description = document.querySelector("#weather-description");
   description.innerHTML = response.data.condition.description;
-   let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.city;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
   let windElement = document.querySelector("#windSpeed");
   windElement.innerHTML = Math.round(response.data.wind.speed);
-let humidityElement = document.querySelector("#humidity");
-humidityElement.innerHTML = response.data.temperature.humidity;
-let iconElement = document.querySelector("#icon");
-iconElement.setAttribute(
-  "src",
-  `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
-);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
   iconElement.setAttribute("alt", response.data.condition.description);
 }
 
-  let now = new Date();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[now.getDay()];
-  let hours = now.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = now.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let am_pm = now.getHours() >= 12 ? " PM" : "AM";
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let am_pm = now.getHours() >= 12 ? " PM" : "AM";
 
-  let dateElement = document.querySelector("#currentDate");
-  dateElement.innerHTML = `${day}, ${hours}:${minutes} ${am_pm}`;
+let dateElement = document.querySelector("#currentDate");
+dateElement.innerHTML = `${day}, ${hours}:${minutes} ${am_pm}`;
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastDays = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+
+  let forecastHTML = `<div class="row">`;
+
+  forecastDays.forEach(function (forecastDay) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forecast-date">${forecastDay}
+                </div>
+               <img src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-day.png" alt=""/>
+              <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temp-max">18°</span>
+                 <span class="weather-forecast-temp-min">12°</span>
+                 </div>
+            </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 
 
 function displayFahrenheitTemperature(event) {
@@ -72,11 +94,6 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-
-
-
-
-
 function search(city) {
   let apiKey = "3oed67abe28tbf31a403d1a6050a989a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -89,8 +106,10 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
 search("New York");
+
+
+displayForecast();
